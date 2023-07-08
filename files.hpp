6304177da07bdd10f5499912b014a0d530f4c6e5
DarkCat5501 <viccente512@gmail.com>
2023-07-08 00:19:28 -0300

    Added file reading function

---
+++ files.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <exception>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+inline std::string read_file(const std::string& file_path) {
+  std::cout<<"rading file:" << file_path << std::endl; 
+  std::fstream file;
+	file.open(file_path, std::ios::in);
+	std::stringstream buffer;
+	if(file.is_open()) buffer << file.rdbuf();
+  return buffer.str();
+}
